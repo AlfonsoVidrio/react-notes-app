2626efe8625c5c52fe1fdb883a571e31580487b9
AlfonsoVidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2024-04-05 02:49:17 -0600

    feat: Add elements to EditNotes page
    Added header with back button, save button, and delete button, and form with title input and details textarea

--- Notes App/src/pages/EditNotes.jsx
+++ Notes App/src/pages/EditNotes.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom"
+import { IoIosArrowBack } from 'react-icons/io';
+import { RiDeleteBin6Line } from "react-icons/ri";
+
 export const EditNotes = () => {
   return (
-    <div>EditNotes</div>
+    <section>
+      <header className="create-note__header">
+        <Link to='/' className="btn"><IoIosArrowBack/></Link>
+        <button className="btn lg primary">Guardar</button>
+        <button className="btn lg danger"><RiDeleteBin6Line/></button>
+      </header>
+      <form action="" className="create-note__form">
+        <input type="text" placeholder="Titulo" autoFocus/>
+        <textarea rows="28" placeholder="detalles de la nota..."></textarea>
+      </form>
+    </section>
   )
 }
