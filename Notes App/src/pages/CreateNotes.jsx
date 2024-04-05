396600ff91e7a8d99146b5dd1e418c3952e779e8
AlfonsoVidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2024-04-05 02:43:29 -0600

    feat: Add elements to CreateNotes page
    Added header with back button and save button, and form with title input and details textarea

--- Notes App/src/pages/CreateNotes.jsx
+++ Notes App/src/pages/CreateNotes.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+import { IoIosArrowBack } from 'react-icons/io';
+
 export const CreateNotes = () => {
   return (
-    <div>CreateNotes</div>
+    <section>
+      <header className="create-note__header">
+        <Link to='/' className="btn"><IoIosArrowBack/></Link>
+        <button className="btn lg primary">Guardar</button>
+      </header>
+      <form action="" className="create-note__form">
+        <input type="text" placeholder="Titulo" autoFocus/>
+        <textarea rows="28" placeholder="detalles de la nota..."></textarea>
+      </form>
+    </section>
   )
 }
